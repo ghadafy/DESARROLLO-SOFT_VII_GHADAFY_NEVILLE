aed49f6f71a84879a303482fe62b9436a4c55a7e
Ghadafy Neville <kadaneville@gmail.com>
2024-09-03 20:14:42 -0500

    Problema 2

--- PARCIALES/PARCIAL_1/funciones_tienda.php
+++ PARCIALES/PARCIAL_1/funciones_tienda.php
@@ -1,12 +1,24 @@
 <?php
 function calcular_descuento($total_compra)
 {
+
+    if ($total_compra < 100) {
+        return $total_compra;
+    } elseif ($total_compra >= 100 && $total_compra <= 500) {
+        return $total_compra * 0.05;
+    } elseif ($total_compra >= 501 && $total_compra <= 1000) {
+        return $total_compra * 0.10;
+    } elseif ($total_compra > 1000) {
+        return $total_compra * 0.15;
+    }
 }
 
 function aplicar_impuesto($subtotal)
 {
+    return $subtotal * 07;
 }
 
 function calcular_total($subtotal, $descuento, $impuesto)
 {
+    return $subtotal - $descuento + $impuesto;
 }
